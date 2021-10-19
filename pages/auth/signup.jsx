31eaa56686b112290e48a5c915ba27ba4863b2ed
ahmad940 <ahmadmuhammadmak5@gmail.com>
2021-10-19 13:52:53 +0100

    setting up directories and more

---
+++ pages/auth/signup.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const SignUp = ({}) => {
+  return (
+    <>
+      <h1>Sign Up Page</h1>
+    </>
+  )
+}
+
+export default SignUp
---
+++ store/modules/auth.js
@@ -0,0 +1,16 @@
+// types
+
+// initial state
+const initialState = {}
+
+// reducer
+const AuthReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state
+  }
+}
+
+//actions
+
+// thunks
